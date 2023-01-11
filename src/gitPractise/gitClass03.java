1da57e5ac371c8c076a7167402a5b29c4775e6a9
Marija Tatalovic <marija.tatalovic@yahoo.com>
2023-01-11 03:21:11 +0100

    v3

--- src/gitPractise/gitClass03.java
+++ src/gitPractise/gitClass03.java
@@ -3,5 +3,6 @@ package gitPractise;
 public class gitClass03 {
     public static void main(String[] args) {
         System.out.println("dfjkjsdkfjalksdjfkaj");
+        System.out.println("123456789");
     }
 }
